89b072dfdf44e255c4ef700fb808fe7491c40847
salem7385 <sa256483@gmail.com>
2026-04-20 12:27:46 +0000

    First Commit: Tire Receipt DocType and Structure

---
+++ tire_recycling/tire_recycling/doctype/tire_receipt/tire_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Factory admin and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tire Receipt", {
+// 	refresh(frm) {
+
+// 	},
+// });
