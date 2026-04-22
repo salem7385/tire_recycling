cf440c9c9b8434e4a1c1340a38ce4bf761f4af2e
salem7385 <sa256483@gmail.com>
2026-04-22 08:20:46 +0000

    update script on tire_receipt.js

--- tire_recycling/tire_recycling/doctype/tire_receipt/tire_receipt.js
+++ tire_recycling/tire_recycling/doctype/tire_receipt/tire_receipt.js
@@ -9,6 +9,14 @@ frappe.ui.form.on('Tire Receipt', {
 
 function calculate_net(frm) {
     if (frm.doc.gross_weight && frm.doc.tare_weight) {
-        frm.set_value('net_weight', frm.doc.gross_weight - frm.doc.tare_weight);
+        let net = frm.doc.gross_weight - frm.doc.tare_weight;
+        frm.set_value('net_weight', net);
+
+        // تحديث الكمية في أول سطر بالجدول تلقائياً
+        if (frm.doc.items && frm.doc.items.length > 0) {
+            let row = frm.doc.items[0];
+            frappe.model.set_value(row.doctype, row.name, 'qty_received', net);
+            frm.refresh_field('items');
+        }
     }
 }
